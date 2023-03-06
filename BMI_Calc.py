b2ce5538ef6fcd0513b68bdfe720f513877c0f78
Michelle Hardin <93739119+michellehardin@users.noreply.github.com>
2023-03-05 19:18:33 -0600

    Update

--- BMI_Calc.py
+++ BMI_Calc.py
@@ -6,11 +6,7 @@ class BMI_Calculator:
     def __init__(self, height_ft, height_inches, weight):
         self.height = (((height_ft * 12) + height_inches) * 0.025) ** 2
         self.weight = 0.45 * weight
-        self.BMI = 0
-
-    def calc_BMI(self):
         self.BMI = self.weight / self.height
-        print("BMI: ", self.BMI)
 
     def determine(self):
         if self.BMI < 18.5:
@@ -25,5 +21,4 @@ class BMI_Calculator:
             print("Uh-oh")
 
 patient = BMI_Calculator(height_ft, height_inches, weight)
-patient.calc_BMI()
 patient.determine()
