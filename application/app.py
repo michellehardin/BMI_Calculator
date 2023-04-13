21c9ef120bb2ca363a70930faf153282ec127eaf
Michelle Hardin <93739119+michellehardin@users.noreply.github.com>
2023-04-13 16:16:57 -0500

    Updated app

--- application/app.py
+++ application/app.py
@@ -6,9 +6,22 @@ app = Flask(__name__)
 
 def calculate(): 
     bmi = 0
+    category = ''
     if request.method=='POST' and 'weight' in request.form and 'height_ft'in request.form and 'height_inch' in request.form:
         w = float(request.form.get('weight'))
         h_ft = float(request.form.get('height_ft'))
         h_in = float(request.form.get('height_inch'))
         bmi = round((0.45 * w)/((((h_ft * 12) + h_in) * 0.025) ** 2), 2)
-    return render_template('index.html', bmi=bmi)
+        if (bmi > 0 and bmi < 18.5):
+            category = "Underweight"
+        elif (bmi >= 18.5 and bmi < 25):
+            category = "Normal"
+        elif (bmi >= 25 and bmi < 30):
+            category = "Overweight"
+        elif (bmi >= 30):
+            category = "Obese"
+        else:
+            category = "ERROR"
+
+    return render_template('index.html', bmi=bmi, category=category)
+
